8c8439c80b47df65f40c5e299f2ee98794203daa
linfangzhi <linfangzhi@163.com>
2018-01-30 12:29:18 +0800

    emmmm

--- one.py
+++ one.py
@@ -1,4 +1,4 @@
 import os
-os.system('sudo yum install epel-release')
-os.system('sudo yum install python34')
+os.system('sudo -y yum install epel-release')
+os.system('sudo -y yum install python34')
 os.system('python3 two.py')
