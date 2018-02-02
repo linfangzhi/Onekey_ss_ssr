c33ed98e95b8e4fb37159a95057bd7d625324249
linfangzhi <linfangzhi@163.com>
2018-02-02 10:23:04 +0800

    emmmm

--- onekey.sh
+++ onekey.sh
@@ -3,4 +3,4 @@ yum -y install git
 git clone https://github.com/linfangzhi/vultr-onekey-ss-ssr
 yum -y install epel-release
 yum -y install python34
-python3 /root/vultr-onekey-ss-ssr/too_young.py
+python3 vultr-onekey-ss-ssr/too_young.py
